0ff9cbaf3d72b6a09559e07507ab486a53bc267f
Takeaki Oura <rtm6@me.com>
2017-12-01 02:14:33 +0900

    starting-controller-success

---
+++ app/controllers/starting_controller.rb
@@ -0,0 +1,5 @@
+class StartingController < ApplicationController
+  def index
+    render :text => "Server Starting OK"
+  end
+end
---
+++ test/controllers/starting_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StartingControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
